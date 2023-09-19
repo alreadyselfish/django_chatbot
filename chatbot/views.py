5d0c2c27f487a9c5cc89e94203cf02746a28f8ee
Sanskar Tiwari <103674304+alreadyselfish@users.noreply.github.com>
2023-09-19 19:56:04 +0530

    first commit

---
+++ chatbot/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+import openai
+
+from django.contrib import auth
+from django.contrib.auth.models import User
+from .models import Chat
+
+from django.utils import timezone
+
+the_prompt = "Your name is Krishi-Mitra. Every time you start to talk, you will address yourself as that. You will strictly talk about and give direct solutions to agriculture related issues. You will add 'HINDI' language quotes in your answers. If I ask you any question of any topic outside the topic of agriculture just repond with this 'Sorry, I am Krishi-Mitra and I can only help you with your agriculture related queries and not anything else. I recommend you google your queries or you can ask other AI language models like myself.'"
+my_key = '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' #abc api_key 5$ trial
+openai.api_key = my_key
+
+def ask_openai(message):
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": the_prompt},
+            {"role": "user", "content": message},
+        ]
+    )
+    
+    answer = response.choices[0].message.content.strip()
+    return answer
+
+# Create your views here.
+def chatbot(request):
+    chats = Chat.objects.filter(user=request.user)
+
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = ask_openai(message)
+
+        chat = Chat(user=request.user, message=message, response=response, created_at=timezone.now())
+        chat.save()
+        return JsonResponse({'message': message, 'response': response})
+    return render(request, 'chatbot.html', {'chats': chats})
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('chatbot')
+        else:
+            error_message = 'Invalid username or password'
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            try:
+                user = User.objects.create_user(username, email, password1)
+                user.save()
+                auth.login(request, user)
+                return redirect('chatbot')
+            except:
+                error_message = 'Error creating account'
+                return render(request, 'register.html', {'error_message': error_message})
+        else:
+            error_message = 'Password dont match'
+            return render(request, 'register.html', {'error_message': error_message})
+    return render(request, 'register.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('login')
